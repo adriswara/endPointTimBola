17aa1f22008b46a959a8702fa1b8120b58202460
adriswara <chevellerz454@yahoo.com>
2025-07-30 09:06:36 +0700

    report get finished

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4,8 +4,45 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Report;
+use Illuminate\Support\Facades\DB;
+
 
 class ReportController extends Controller
 {
-    //
+    // Your methods for handling Report-related requests will go here
+    // For example:
+
+    public function showAll()
+    {
+        $results = DB::table('skor')
+            ->join('jadwal', 'skor.idJadwal', '=', 'jadwal.id')
+            ->select(
+                'skor.totalSkor',
+                'skor.pemainYangMencetakGol',
+                'skor.waktuTerjadinyaGol',
+                'jadwal.tanggalPertandingan',
+                'jadwal.timRumah',
+                'jadwal.timTamu',
+                'jadwal.waktuPertandingan',
+                DB::raw('(SELECT SUM(s.totalSkor) FROM skor s WHERE s.idTim = jadwal.timRumah) as totalkemenanganhome'),
+                DB::raw('(SELECT SUM(s2.totalSkor) FROM skor s2 WHERE s2.idTim = jadwal.timTamu) as totalkemenanganaway'),
+                DB::raw('IF(
+                (SELECT SUM(s.totalSkor) FROM skor s WHERE s.idTim = jadwal.timRumah) > 
+                (SELECT SUM(s2.totalSkor) FROM skor s2 WHERE s2.idTim = jadwal.timTamu),
+                "Tim Home Menang",
+                "Tim Away Menang"
+            ) as statusKemenangan')
+            )
+            ->get();
+
+        return response()->json($results);
+    }
+
+    public function show($id)
+    {
+        // Logic to retrieve and return a specific report by ID
+    }
+
+    // Add more methods as needed for your application
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\TimController;
 use App\Http\Controllers\JadwalController;
 use App\Http\Controllers\SkorController;
 use App\Http\Controllers\PemainController;
+use App\Http\Controllers\ReportController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,3 +14,4 @@ Route::get('/pemain/showAll', [PemainController::class, 'showAll']);
 Route::get('/tim/showAll', [TimController::class, 'showAll']);
 Route::get('/jadwal/showAll', [JadwalController::class, 'showAll']);
 Route::get('/skor/showAll', [SkorController::class, 'showAll']);
+Route::get('/report/showAll', [ReportController::class, 'showAll']);
